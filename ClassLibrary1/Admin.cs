da4ce93a2e77ad50d17c67bd067ac10c6c0aa2a2
Louisa Lee <lawheezha@dhcp-10-105-154-124.wireless.northwestern.private>
2018-04-13 20:40:06 -0500

    Reformat Test Classes

--- ClassLibrary1/Admin.cs
+++ ClassLibrary1/Admin.cs
@@ -13,12 +13,11 @@ namespace tsuro
     }
     public class Admin:IAdmin
     {
-        public bool legalPlay(SPlayer p, Board b, Tile t)
+        private bool tileInHand(SPlayer p, Tile t)
         {
-            List<Tile> hand = new List<Tile>();
-            hand = p.returnHand();
+            List<Tile> hand = p.returnHand();
             //if the tile is not (a possibly rotated version) of the tiles of the player
-            if(hand == null)//if there are no tiles in the players hand
+            if (hand == null)//if there are no tiles in the players hand
             {
                 return false;
             }
@@ -33,10 +32,18 @@ namespace tsuro
                         {
                             return true;
                         }
-                    } 
+                    }
                 }
                 return false;
             }
+        }
+        private bool playerEliminated(SPlayer p, Board b, Tile t)
+        {
+            throw new NotImplementedException();
+        }
+        public bool legalPlay(SPlayer p, Board b, Tile t)
+        {
+            return (tileInHand(p, t) && playerEliminated(p, b, t));
         }
 
         public TurnResult playATurn(List<Tile> pile, List<SPlayer> inGamePlayers, List<SPlayer> eliminatedPlayers,
