0081de8bd2b67a87e8f26215c2647e6fc3ad9be5
jonodeno <jondenose@gmail.com>
2018-04-15 19:00:32 -0500

    jon edits

--- ClassLibrary1/Board.cs
+++ ClassLibrary1/Board.cs
@@ -9,12 +9,20 @@ namespace tsuro
         //returns true if the move was valid and the tile was placed
         //returns false if there was a tile there already 
         bool placeTile(int row, int col, Tile t);
+        //registers players in the beginning of the game
+        void registerPlayer(SPlayer p);
+        void eliminatePlayer(SPlayer p);
     }
 
     public class Board:IBoard
     {
+        //details of tiles on the board
         Tile[,] grid = new Tile[6, 6];
-        SPlayer onBoard;
+
+        List<SPlayer> onBoard;
+        List<SPlayer> eliminated;
+        //board must track the details of players and tiles on a board
+
         public bool placeTile(int row, int col, Tile t)
         {
             throw new NotImplementedException();
