e7cf53fa12e06ad76bb6b4e12c92a68d9956a5b7
Louisa Lee <lawheezha@dhcp-10-105-154-124.wireless.northwestern.private>
2018-05-06 10:09:29 -0500

    Extract Base Class for automated players

--- ClassLibrary1/Admin.cs
+++ ClassLibrary1/Admin.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-//using System.box.Collections;
 using System.Collections.Generic;
 using System.IO;
 
--- ClassLibrary1/MostSymmetricPlayer.cs
+++ ClassLibrary1/MostSymmetricPlayer.cs
@@ -5,74 +5,8 @@ using System.Text;
 
 namespace tsuro
 {
-    public class MostSymmetricPlayer : IPlayers
+    public class MostSymmetricPlayer : AutomatedPlayer, IPlayers
     {
-        string name = "";
-        List<string> allPlayers = new List<string>();
-        string[] validNames = new string[] {"blue","red","green","orange","sienna"
-            ,"hotpink","darkgreen","purple"};
-
-        public string getName()
-        {
-            return name;
-        }
-
-        public void initialize(string playerColor, List<string> allColors)
-        {
-            allPlayers = allColors;
-            if (validNames.Contains(playerColor))
-            {
-                name = playerColor;
-            }
-            else
-            {
-                throw new Exception("not a valid color!");
-            }
-        }
-
-        public Posn placePawn(Board b)
-        {
-            // row is either 0(0 and 1) or 5(4 and 5)
-            // col is either 0(6 and 7) or 5(2 and 3)
-            int[] edgeRows = new int[] { 0, 5 };
-            int[] edgeCols = new int[] { 0, 5 };
-            Dictionary<int, int[]> edgeRowLoc = new Dictionary<int, int[]>();
-            edgeRowLoc.Add(0, new int[] { 0, 1 });
-            edgeRowLoc.Add(5, new int[] { 4, 5 });
-            Dictionary<int, int[]> edgeColLoc = new Dictionary<int, int[]>();
-            edgeColLoc.Add(0, new int[] { 6, 7 });
-            edgeColLoc.Add(5, new int[] { 2, 3 });
-
-            for (int i = 0; i < edgeRows.Length; i++)
-            {
-                for (int j = 0; j < 6; j++)
-                {
-                    foreach (int loc in edgeRowLoc[edgeRows[i]])
-                    {
-                        if (!b.locationOccupied(edgeRows[i], j, loc))
-                        {
-                            return new Posn(edgeRows[i], j, loc);
-                        }
-                    }
-                }
-            }
-
-            for (int i = 0; i < edgeCols.Length; i++)
-            {
-                for (int j = 0; j < 6; j++)
-                {
-                    foreach (int loc in edgeColLoc[edgeCols[i]])
-                    {
-                        if (!b.locationOccupied(j, edgeCols[i], loc))
-                        {
-                            return new Posn(j, edgeCols[i], loc);
-                        }
-                    }
-                }
-            }
-            throw new Exception("Edges of Board are Full.");
-
-        }
         public Tile playTurn(Board b, List<Tile> playerHand, int numTilesInDrawPile)
         {
             //ordered from least to most symmetric
@@ -143,11 +77,6 @@ namespace tsuro
 
                 return validMoves[0];
             }
-
-        }
-        public void endGame(Board b, List<string> allColors)
-        {
-            throw new NotImplementedException();
 
         }
     }
