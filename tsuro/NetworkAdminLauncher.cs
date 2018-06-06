d22fe7fa23fc3a4609192f543ba9967a2c402f3d
Louisa Lee <lawheezha@Louisas-MacBook-Pro.local>
2018-06-06 00:36:56 -0500

    NETWORK PLAYER NOW PLAYS ON TOURNAMENT :D

--- tsuro/NetworkAdminLauncher.cs
+++ tsuro/NetworkAdminLauncher.cs
@@ -3,19 +3,20 @@
 //using System.Net.Sockets;
 //using System.Collections.Generic;
 //using System.Text;
+//using System.IO;
 
 //namespace tsuro
 //{
 //	public class NetworkAdminLauncher
-//	{      
+//	{
 //		static void Main(String[] args)
 //		{
 //			IPlayer ourPlayer = new MostSymmetricPlayer("Jerry");
 
 //			NetworkAdmin networkPlayer = new NetworkAdmin(ourPlayer);
-            
+
 //			string ipServer = Dns.GetHostEntry("localhost").AddressList[0].ToString();
-//            int port = 12345;
+//			int port = 12345;
 //			byte[] dataBuffer = new byte[1024];
 
 //			string myIP = Dns.GetHostEntry(Dns.GetHostName()).AddressList[0].ToString();
@@ -24,49 +25,29 @@
 //			// Connect to server
 //			try
 //			{
-//				// Establish remote endpoint for socket
-
+//				// Establish remote endpoint for socket            
 //				TcpClient client = new TcpClient();
-//				client.ReceiveBufferSize = 5096;
 //				client.Connect("localhost", port);
 
-
-//    			while (true)
-//    			{
-//					//Console.WriteLine("beginning of loop");
-
+//				while (true)
+//				{
 
 //					NetworkStream stream = client.GetStream();
-//					// Get response
-//					dataBuffer = new byte[1024];
+//					var reader = new StreamReader(stream);
+//                    string line;
+//                    while ((line = reader.ReadLine()) != null)
+//                    {
+//                        Console.WriteLine(line);
+//						String responseToServer = networkPlayer.interpretQuery(line) + "\n";
+//                        //Console.WriteLine(responseToServer);
 
-//					StringBuilder responseData = new StringBuilder();
-//					int numBytesRead = 0;
-
-//                    // Process data in buffer - query is until we see a new line,
-//                    // if we don't see a new line, we need to read more
-//					do
-//					{
-//						numBytesRead = stream.Read(dataBuffer, 0, dataBuffer.Length);
-//						responseData.Append(Encoding.ASCII.GetString(dataBuffer, 0, numBytesRead));
-//					} while (stream.DataAvailable);
-                    
-//					//Int32 numBytesData = stream.Read(dataBuffer, 0, dataBuffer.Length);
-//					//responseData = System.Text.Encoding.ASCII.GetString(dataBuffer, 0, numBytesData);
-//					Console.WriteLine(responseData);
-
-//					dataBuffer = new byte[1024];
-//					String responseToServer = networkPlayer.interpretQuery(responseData.ToString()) + "\n";
-//					Console.WriteLine(responseToServer);
-
-//					dataBuffer = System.Text.Encoding.ASCII.GetBytes(responseToServer);
-//				    stream.Write(dataBuffer, 0, dataBuffer.Length);
-//					stream.Flush();
-
-//    				//stream.Close();
-//    				//client.Close();
-//			    }
-
+//                        byte[] dataBufferOut = System.Text.Encoding.ASCII.GetBytes(responseToServer);
+//                        stream.Write(dataBufferOut, 0, dataBufferOut.Length);
+//                        stream.Flush();
+//                    }
+//					stream.Close();
+//                    client.Close();
+//				}
 
 //			}
 //			catch (Exception e)
